03318817c30490ea50e0053fd453accb456fd333
Anders Karlsen <anderska@nmbu.no>
2020-01-07 14:35:03 +0100

    Added skeleton draft for animals

---
+++ src/biosim/animals.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Kåre Johnsen & Anders Karlsen"
+__email__ = "kajohnse@nmbu.no & anderska@nmbu.no"
+
+class Animals:
+    def __init__(self):
+
+
+    def eat(self):
+        """This function makes the animal eat
+        """
