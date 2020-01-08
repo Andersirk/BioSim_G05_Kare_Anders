989989995ab1ba34b8c81de904a33c2ba33ae519
Anders Karlsen <anderska@nmbu.no>
2020-01-08 12:41:35 +0100

    Resolved mergeconflict

--- tests/test_cell_topography.py
+++ tests/test_cell_topography.py
@@ -3,10 +3,12 @@
 __author__ = "Kåre Johnsen & Anders Karlsen"
 __email__ = "kajohnse@nmbu.no & anderska@nmbu.no"
 
-@pytest.fixture
-def jungle_cell():
-    """Return a simple island for used in various tests below"""
-    return BioSim(island_map="OOOO\nOJSO\nOOOO", ini_pop=[], seed=1)
+import src.biosim.cell_topography as topo
+
+
+def test_test():
+    ke=topo.Jungle()
+    assert ke.fodder == 800
 
 
 # cell_topography_tests
