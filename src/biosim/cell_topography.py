7e927812ee67e2525196af75eace5ec837d41e69
Anders Karlsen <anderska@nmbu.no>
2020-01-08 11:15:48 +0100

    finished a draft for cell topography

--- src/biosim/cell_topography.py
+++ src/biosim/cell_topography.py
@@ -4,12 +4,10 @@ __author__ = "Kåre Johnsen & Anders Karlsen"
 __email__ = "kajohnse@nmbu.no & anderska@nmbu.no"
 
 
-
 class Topography:
     def __init__(self):
         self.accessible = False
         self.animals = []
-        self.fodder = None
 
     def decrease_fodder(self, decrease_amount):
         if decrease_amount <= self.fodder:
@@ -22,80 +20,99 @@ class Topography:
         elif self.fodder <= 0:
             return 0
 
-    def remove_animal(self):
-        pass
+    def remove_animal(self, animal):
+        self.animals.remove(animal)
 
-    def add_animal(self):
-        pass
+    def add_animal(self, animal):
+        self.animals.append(animal)
 
     def current_fodder(self):
-        pass
+        return self.fodder
+
+    def current_occupants(self):
+        herbivore_count = 0
+        carnivore_count = 0
+        for animal in self.animals:
+            if animal.__class__.__name__ == "Herbivore":
+                herbivore_count += 1
+            elif animal.__class__.__name__ == "Carnivore":
+                carnivore_count += 1
+        return {"Herbivores": herbivore_count,
+                "Carnivores": carnivore_count,
+                "Total": len(self.animals)}
 
 
 class Jungle(Topography):
     def __init__(self):
         super().__init__()
         self.accessible = True
-        self.jungle_max_fodder = 800
+        self.f_max_jungle = 800
+        self.fodder = self.f_max_jungle
 
     def set_attributes(self):
         pass
 
     def increase_fodder(self):
         """Increases """
-        self.fodder = self.jungle_max_fodder
+        self.fodder = self.f_max_jungle
 
 class Savanna(Topography):
     def __init__(self):
         super().__init__()
         self.accessible = True
+        self.f_max_savanna = 300
+        self.alpha = 0.3
+        self.fodder = self.f_max_savanna
 
     def set_attributes(self):
         pass
 
     def increase_fodder(self):
-        """Increases """
-        pass
+        self.fodder += self.alpha * (self.f_max_savanna - self.fodder)
+
 
 
 class Desert(Topography):
     def __init__(self):
         super().__init__()
         self.accessible = True
+        self.fodder = 0
 
 
 class Mountain:
     def __init__(self):
         self.accessible = False
+        self.fodder = 0
 
 
 class Ocean:
     def __init__(self):
         self.accessible = False
+        self.fodder = 0
 
 
 def create_map(island_map):
-     """ Creates a dictionary where the keys are coordinates and values
-     are a class with the relevant topography category.
+    """ Creates a dictionary where the keys are coordinates and values
+    are a class with the relevant topography category.
 
-     :param island_map:
-     :return dictinary,
-     """
+    :param island_map:
+    :return dictinary,
+    """
     island_map_no_spaces = island_map.replace(" ", "")
     x, y = 0, -1
-    map = {}
+    raster_model = {}
     for number, landscape_code in enumerate(island_map_no_spaces):
         y += 1
         if landscape_code == "D":
-            map[(x, y)] = Desert()
+            raster_model[(x, y)] = Desert()
         elif landscape_code == "J":
-            map[(x, y)] = Jungle()
+            raster_model[(x, y)] = Jungle()
         elif landscape_code == "S":
-            map[(x, y)] = Savanna()
+            raster_model[(x, y)] = Savanna()
         elif landscape_code == "O":
-            map[(x, y)] = Ocean()
+            raster_model[(x, y)] = Ocean()
         elif landscape_code == "M":
-            map[(x, y)] = Mountain()
+            raster_model[(x, y)] = Mountain()
         elif landscape_code == "\n":
             y = -1
             x += 1
@@ -103,6 +120,30 @@ def create_map(island_map):
             raise ValueError
         elif y == 0 and landscape_code != "O":
             raise ValueError
-        if landscape_code not in ["O", "M", "J", "S", "D"]:
+        if landscape_code not in ["O", "M", "J", "S", "D", "\n"]:
             raise ValueError
-    return map
+    return raster_model
+
+
+
+if __name__ == "__main__":
+    geogr = """\
+                OOOOOOOOOOOOOOOOOOOOO
+                OOOOOOOOSMMMMJJJJJJJO
+                OSSSSSJJJJMMJJJJJJJOO
+                OSSSSSSSSSMMJJJJJJOOO
+                OSSSSSJJJJJJJJJJJJOOO
+                OSSSSSJJJDDJJJSJJJOOO
+                OSSJJJJJDDDJJJSSSSOOO
+                OOSSSSJJJDDJJJSOOOOOO
+                OSSSJJJJJDDJJJJJJJOOO
+                OSSSSJJJJDDJJJJOOOOOO
+                OOSSSSJJJJJJJJOOOOOOO
+                OOOSSSSJJJJJJJOOOOOOO
+                OOOOOOOOOOOOOOOOOOOOO"""
+
+
+    mappp = create_map(geogr)
+    for key, value in mappp.items():
+        if value.__class__.__name__ != "Ocean" and value.__class__.__name__ != "Mountain":
+            print (f'Coordinate: {key}, has {value.fodder} fodder and the geography is {value.__class__.__name__}, {value.current_occupants()}')
