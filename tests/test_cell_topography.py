ecc991944bc2d733e536244d2a48a89c37a15174
Kaare Johnsen <kajohnse@nmbu.no>
2020-01-08 12:39:34 +0100

    Made some test for cell_topo

--- tests/test_cell_topography.py
+++ tests/test_cell_topography.py
@@ -7,3 +7,20 @@ __email__ = "kajohnse@nmbu.no & anderska@nmbu.no"
 def jungle_cell():
     """Return a simple island for used in various tests below"""
     return BioSim(island_map="OOOO\nOJSO\nOOOO", ini_pop=[], seed=1)
+
+
+# cell_topography_tests
+
+def current_occupants_int_test
+    assert type(herbivore_count) == int and type(carnivore_count) == int
+
+def current_occupants_positive_test
+    assert type(herbivore_count) => 0 and type(carnivore_count) => 0
+
+def animal_list_test
+    assert type(animal) == list
+
+
+
+
+
