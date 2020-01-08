80c3a135069a9587c251884ac2e1ec5922fa0a9e
Anders Karlsen <anderska@nmbu.no>
2020-01-08 12:54:43 +0100

    Some more test edits

--- tests/test_cell_topography.py
+++ tests/test_cell_topography.py
@@ -10,16 +10,16 @@ def test_test():
     ke=topo.Jungle()
     assert ke.fodder == 800
 
-#
-# def test_current_occupants_int_test():
-#     instance = topo.Topography()
-#     assert type(instance.herbivore_count) == int and type(instance.carnivore_count) == int
-#
-#
-# def test_current_occupants_positive_test():
-#     instance = topo.Topography()
-#     assert type(instance.herbivore_count) >= 0 and type(instance.carnivore_count) >= 0
-#
+
+def test_current_occupants_int_test():
+    instance = topo.Topography()
+    assert type(instance.current_occupants()["Herbivores"]) == int and type(instance.current_occupants()["Carnivores"]) == int
+
+
+def test_current_occupants_positive_test():
+    instance = topo.Topography()
+    assert instance.current_occupants()["Herbivores"] >= 0 and instance.current_occupants()["Carnivores"] >= 0
+
 
 def test_animal_list_test():
     instance = topo.Topography()
