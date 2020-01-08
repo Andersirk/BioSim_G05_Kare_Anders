9ecdfc94c125a57e7e4302475b7d9af9c2183066
Anders Karlsen <anderska@nmbu.no>
2020-01-08 12:48:59 +0100

    Test edits

--- tests/test_cell_topography.py
+++ tests/test_cell_topography.py
@@ -10,17 +10,20 @@ def test_test():
     ke=topo.Jungle()
     assert ke.fodder == 800
 
+#
+# def test_current_occupants_int_test():
+#     instance = topo.Topography()
+#     assert type(instance.herbivore_count) == int and type(instance.carnivore_count) == int
+#
+#
+# def test_current_occupants_positive_test():
+#     instance = topo.Topography()
+#     assert type(instance.herbivore_count) >= 0 and type(instance.carnivore_count) >= 0
+#
 
-# cell_topography_tests
-
-def current_occupants_int_test
-    assert type(herbivore_count) == int and type(carnivore_count) == int
-
-def current_occupants_positive_test
-    assert type(herbivore_count) => 0 and type(carnivore_count) => 0
-
-def animal_list_test
-    assert type(animal) == list
+def test_animal_list_test():
+    instance = topo.Topography()
+    assert type(instance.animals) == list
 
 
 
