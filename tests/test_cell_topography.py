fde834f737d373587fb9e5baf8ef38869c7556a6
Anders Karlsen <anderska@nmbu.no>
2020-01-08 13:36:36 +0100

    Tests for decrease fodder

--- tests/test_cell_topography.py
+++ tests/test_cell_topography.py
@@ -6,10 +6,29 @@ __email__ = "kajohnse@nmbu.no & anderska@nmbu.no"
 import src.biosim.cell_topography as topo
 
 
-def test_test():
-    ke=topo.Jungle()
-    assert ke.fodder == 800
+def test_decrease_fodder_abundance():
+    instance = topo.Topography()
+    instance.fodder = 400
+    decrease_amount = instance.decrease_fodder(200)
+    assert decrease_amount == 200
+    assert instance.current_fodder() == 200
 
+
+def test_decrease_fodder_scarce():
+    instance = topo.Topography()
+    instance.fodder = 100
+    decrease_amount = instance.decrease_fodder(200)
+    assert decrease_amount == 100
+    assert instance.current_fodder() == 0
+
+
+def test_decrease_fodder_zero():
+    instance = topo.Topography()
+    instance.fodder = 0
+    decrease_amount = instance.decrease_fodder(200)
+    assert decrease_amount == 0
+    assert instance.current_fodder() == 0
+    
 
 def test_current_occupants_int_test():
     instance = topo.Topography()
