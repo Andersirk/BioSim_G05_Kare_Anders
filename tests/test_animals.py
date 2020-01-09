075d5e6989bde01bd610d961f0ef83e99478db66
Anders Karlsen <anderska@nmbu.no>
2020-01-09 15:55:06 +0100

    Test for fitness when weight = 0

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -5,6 +5,7 @@ __email__ = "kajohnse@nmbu.no & anderska@nmbu.no"
 
 import unittest
 import src.biosim.animals as ani
+import pytest
 
 
 class AnimalsTest(unittest.TestCase):
@@ -15,6 +16,16 @@ class AnimalsTest(unittest.TestCase):
         self.assertAlmostEqual(ani.Herbivores.birth_weight(ani.Herbivores()), 10)
 
 
+
+@pytest.fixture
+def herbivore_zero_weight():
+    instance = ani.Herbivores()
+    instance.weight = 0
+    return instance
+
+
+def test_fitnesslevel_is_zero(herbivore_zero_weight):
+    assert herbivore_zero_weight.fitness == 0
 
 
 
