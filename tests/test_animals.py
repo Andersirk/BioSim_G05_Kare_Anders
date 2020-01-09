b090f508585cfd4b6e399121b7813359d4296644
Kaare Johnsen <kajohnse@nmbu.no>
2020-01-09 15:41:57 +0100

    brith_weight test

--- tests/test_animals.py
+++ tests/test_animals.py
@@ -2,3 +2,25 @@
 
 __author__ = "Kåre Johnsen & Anders Karlsen"
 __email__ = "kajohnse@nmbu.no & anderska@nmbu.no"
+
+import unittest
+import src.biosim.animals as ani
+
+
+class AnimalsTest(unittest.TestCase):
+    def test_birthweight_sd0(self):
+        # Test that the birth weight is correct
+        ani.Herbivores.parameters["w_birth"] = 10
+        ani.Herbivores.parameters["sigma_birth"] = 0
+        self.assertAlmostEqual(ani.Herbivores.birth_weight(ani.Herbivores()), 10)
+
+
+
+
+
+
+
+
+
+
+
