05084ccf044dba845bf36487a4176c47ca5f5282
Anders Karlsen <anderska@nmbu.no>
2020-01-12 12:10:01 +0100

    Added a couple of tests for populate island

--- tests/test_island.py
+++ tests/test_island.py
@@ -20,7 +20,7 @@ def standard_map():
     island = Island(geogr)
     return island
 
-def test_populate_island(standard_map):
+def test_populate_island_acceptable_cells(standard_map):
     population = [{'loc': (1, 14),
       'pop': [{'species': 'Herbivore',
                'age': 10, 'weight': 12.5},
@@ -38,24 +38,34 @@ def test_populate_island(standard_map):
     standard_map.populate_island(population)
     assert len(standard_map.raster_model[(1,14)].herbivore_list) == 2
 
+def test_populate_island_ocean_cell_raises_valueerror(standard_map):
+    with pytest.raises(ValueError):
+        population = [{'loc': (1, 3),
+          'pop': [{'species': 'Herbivore',
+                   'age': 10, 'weight': 12.5},
+                  {'species': 'Herbivore',
+                   'age': 9, 'weight': 10.3},
+                  {'species': 'Carnivore',
+                   'age': 5, 'weight': 8.1}]}]
+        standard_map.populate_island(population)
+
+def test_populate_island_nonexistant_coordinates(standard_map):
+    with pytest.raises(ValueError):
+        population = [{'loc': (-1, 3),
+          'pop': [{'species': 'Herbivore',
+                   'age': 10, 'weight': 12.5},
+                  {'species': 'Herbivore',
+                   'age': 9, 'weight': 10.3},
+                  {'species': 'Carnivore',
+                   'age': 5, 'weight': 8.1}]}]
+        standard_map.populate_island(population)
+
+
+
 
 @pytest.fixture
-def surrounding_ocean_cell():
-    geogr = """\
-                OOOOOOOOOOOOOOOOOOOOO
-                OOOOOOOOSMMMMJJJJJJJO
-                OSSSSSJJJJMMJJJJJJJOO
-                OSSSSSSSSSMMJJJJJJOOO
-                OSSSSSJJJJJJJJJJJJOOO
-                OSSSSSJJJDDJJJSJJJOOO
-                OSSJJJJJDDDJJJSSSSOOO
-                OOSSSSJJJDDJJJSOOOOOO
-                OSSSJJJJJDDJJJJJJJOOO
-                OSSSSJJJJDDJJJJOOOOOO
-                OOSSSSJJJJJJJJOOOOOOO
-                OOOSSSSJJJJJJJOOOOOOO
-                OOOOOOOOOOOOOOOOOOOOO"""
-    island = Island(geogr)
+def surrounding_ocean_cell(standard_map):
+    island = standard_map
     occupants = [{'loc': (1, 19),
            'pop': [{'species': 'Herbivore',
                'age': 10, 'weight': 12.5},
@@ -71,8 +81,9 @@ def test_one_option_migration(surrounding_ocean_cell):
 @pytest.fixture
 def mock_ek_and_neighbouring_cells():
     neighbouring_cells = [(11,10),(9,10),(10,11),(10,9)]
-    herbivore_ek = {(11,10): 0}
+    herbivore_ek = {(11,10): 1}
     return neighbouring_cells, herbivore_ek
+
 
 def test_what_cell(standard_map, mock_ek_and_neighbouring_cells):
     neighbouring_cells, herbivore_ek = mock_ek_and_neighbouring_cells
