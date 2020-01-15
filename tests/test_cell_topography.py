655b167fbf44ee645a3fe0bcf8383feae6976d54
Kaare Johnsen <kajohnse@nmbu.no>
2020-01-15 18:27:12 +0100

    test_feeding

--- tests/test_cell_topography.py
+++ tests/test_cell_topography.py
@@ -82,7 +82,7 @@ def test_Ocean_fodder():
 
 @pytest.fixture
 def low_fitness_animals():
-    jungle_cell = topo.Desert()
+    jungle_cell = topo.Jungle()
     herbivore = animals.Herbivores()
     carnivore = animals.Carnivores()
     carnivore.weight, carnivore.age = 1, 1000
@@ -93,35 +93,26 @@ def low_fitness_animals():
     jungle_cell.add_animal(carnivore)
     return jungle_cell
 
-
-#migration
-@pytest.fixture
-def surrounding_ocean_cell():
-    geogr = """\
-                OOOOOOOOOOOOOOOOOOOOO
-                OOOOOOOOSMMMMJJJJJJJO
-                OSSSSSJJJJMMJJJJJJJOO
-                OSSSSSSSSSMMJJJJJJOOO
-                OSSSSSJJJJJJJJJJJJOOO
-                OSSSSSJJJDDJJJSJJJOOO
-                OSSJJJJJDDDJJJSSSSOOO
-                OOSSSSJJJDDJJJSOOOOOO
-                OSSSJJJJJDDJJJJJJJOOO
-                OSSSSJJJJDDJJJJOOOOOO
-                OOSSSSJJJJJJJJOOOOOOO
-                OOOSSSSJJJJJJJOOOOOOO
-                OOOOOOOOOOOOOOOOOOOOO"""
-    island = isle.Island(geogr)
-    occupants = [{'loc': (1, 19),
-           'pop': [{'species': 'Herbivore',
-               'age': 10, 'weight': 12.5},
-              {'species': 'Herbivore',
-               'age': 9, 'weight': 10.3}]}]
-    island.populate_island(occupants)
-    return island
 def test_natural_death_in_all_cells(low_fitness_animals):
     low_fitness_animals.natural_death_all_animals_in_cell()
     assert len(low_fitness_animals.herbivore_list) == 0
+    assert len(low_fitness_animals.carnivore_list) == 0
+
+
+def test_feeding():
+    jungle_cell = topo.Jungle()
+    [jungle_cell.add_animal(animals.Herbivores()) for herbivores in range(250)]
+    [jungle_cell.add_animal(animals.Carnivores()) for carnivores in range(250)]
+    herbivore_fitness_sort = sorted(jungle_cell.herbivore_list,
+                                    key=lambda herbi: herbi.fitness, reverse=True)
+    testani = herbivore_fitness_sort[0]
+    least_fittest_weight = testani.weight
+    jungle_cell.feed_herbivores_in_cell()
+    assert least_fittest_weight == testani.weight
+
+#migration
 
 def test_migrate_all_herb_in_cell_mock_ek():
     pass
+
+
